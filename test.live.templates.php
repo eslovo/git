1f5d3ea6913bfcdf0bd29dc90d2a82e25d107239
Vitaly Lebedev <eslovo@gmail.com>
2019-07-04 15:34:19 +0300

    Testing branching: file test.live... to commit

--- test.live.templates.php
+++ test.live.templates.php
@@ -5,3 +5,23 @@
  * Date: 13.06.2019
  * Time: 20:54
  */
+
+class TEST {
+    /**
+     * @param int $var
+     * @param string $str
+     * @param array $arr
+     * @return array
+     */
+    public function someFunc(int $var, string $str, array $arr) : array
+    {
+
+    }
+
+    protected function someProFunc(int $char, array $arr) : string
+    {
+        foreach ($this->var as $item) {
+
+        }
+    }
+}
